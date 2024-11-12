8d894d99b697be69fe1ebe6da2fe6f69d65fdb63
aleswinkler87 <aleswinkler87@gmail.com>
2024-11-12 19:09:02 +0100

    school

--- main.py
+++ main.py
@@ -1,1 +1,66 @@
-print("fff")
+
+
+"""
+text = "HEllo 2"
+
+file = open("test1.txt", "a")
+file.write(text)
+file.close()
+
+
+file = open("test1.txt", "r")
+output = file.read()
+file.close()
+print(output)
+
+
+
+with open("test2.special", "w") as f:
+    f.writelines(["line1\n", "line2"])
+
+
+with open("test2.special", "r") as f:
+    text = f.readlines()
+    print(text)
+
+"""
+
+"""
+FILE_PATH = "test1.txt"
+
+f = open(FILE_PATH, "w")
+f.write("Hello world I am ales and i want to go home")
+f.close()
+
+"""
+"""# Define the source and destination file paths
+source_file = "source.txt"  # The existing file you want to read from
+destination_file = "filtered_words.txt"  # The new file where results will be written
+
+# Open the source file in read mode and the destination file in write mode
+with open(source_file, 'r') as src, open(destination_file, 'w') as dest:
+    # Read each line in the source file
+    for line in src:
+        # Split each line into words
+        words = line.split()
+
+        # Filter words that have at least 4 letters
+        filtered_words = [word for word in words if len(word) >= 4]
+
+        # Write the filtered words to the destination file, joined by a space
+        dest.write(" ".join(filtered_words) + "\n")"""
+
+
+
+existing_FILE_PATH = "test1.txt"
+new_file = "filtered_words.txt"
+
+with open(existing_FILE_PATH) as src, open(new_file, 'w') as dest:
+    for line in src:
+        words = line.split()
+
+        filtered_words = [word for word in words if len(word) >= 4]
+
+        dest.write(" ".join(filtered_words) + "\n")
+
+
